2ed33c09ef52b2314d48cc8d3b4b8428b2d82412
derkoe <christian.koeberl@gmail.com>
2024-02-21 12:54:01 +0000

    Update awesome cat pic + tagline

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,9 +9,9 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="https://www.publicdomainpictures.net/pictures/20000/velka/cute-cat.jpg"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Mewooooooooooooooooooow!
   <!-- END -->
   
   </div>
