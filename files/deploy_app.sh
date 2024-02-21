4fdac70799dd24419d33de5d4c79ef050e88f8c3
derkoe <christian.koeberl@gmail.com>
2024-02-21 13:10:23 +0000

    fix cat picture width

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,7 +9,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="https://www.publicdomainpictures.net/pictures/20000/velka/cute-cat.jpg"></img></center>
+  <center><img src="https://www.publicdomainpictures.net/pictures/20000/velka/cute-cat.jpg" width="${WIDTH}"></img></center>
   <center><h2>Meow World!</h2></center>
   Mewooooooooooooooooooow!
   <!-- END -->
